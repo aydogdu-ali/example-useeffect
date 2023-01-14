8301732baf1dc5d9c627335cb74056759bf751e1
Ali <ali.ozgur@example.com>
2023-01-15 00:33:22 +0300

    added css and commnets

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
-
-import './App.css';
-import UseEffectExample from './components/UseEffectExample';
+import "./App.css";
+import UseEffectExample from "./components/UseEffectExample";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      
-      <UseEffectExample/>
+        <UseEffectExample />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://tr.reactjs.org/docs/hooks-reference.html#useeffect"
           target="_blank"
           rel="noopener noreferrer"
         >
--- src/components/UseEffectExample.jsx
+++ src/components/UseEffectExample.jsx
@@ -1,32 +1,53 @@
 // useEffect Hook 'u react  component yaşam döngüsünde kaldırma, güncelleme ve çekme gibi durumlarda kullanılır.
 import React from 'react' 
-import { useState, useEffect } from 'react'
-import ListUser from "./ListUser"
+import { useState, useEffect } from 'react' //Kullanacağım Hookları çalışıyorum.
+import ListUser from "./ListUser" // listelediğim componenti import ediyorum.
 
 
 const UseEffectExample = () => {
-    const [ user, setUser] = useState([]) // gelen veri array içinde olduğu için başlangıçı array veriyoruz.
 
-    //Api den veri çekme fonksiyonumu tanımlıyorum
-    // Bildiğiniz gibi bu fonksiyonun çalışması için çağrılması gerekir.
-    // Ancak bunu aşağıda çağırırsak sayfa sonsuz döngüye gider.
-     const url = "https://api.github.com/users";
-    const getUser = ()=>{
-        fetch(url)
-        .then((res)=>res.json())
-        .then((data)=> setUser(data))
-        .catch((err)=> console.log(err))
+  const [user, setUser] = useState([]); // gelen veri array içinde olduğu için başlangıçı array veriyoruz.
 
-    }
+  //Api den veri çekme fonksiyonumu tanımlıyorum
+  // Bildiğiniz gibi bu fonksiyonun çalışması için çağrılması gerekir.
 
-   getUser()
+  const url = "https://api.github.com/users";
 
+  const getUser = () => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setUser(data))
+      .catch((err) => console.log(err));
+  };
+  // Ancak bunu aşağıda çağırırsak sayfa sonsuz döngüye gider.
+  // Bunun engellenmesi için useEffect Hook'unu kullanıyoruz.
+//   getUser();
+//   console.log("veri geldi");
+
+// Bu hook 'un bu şekilde yazılması demek sayfa ilk render olduğunda çalış bir  daha çalışma anlamındadır.
+ useEffect(()=>{
+    getUser()
+ },[])
+
+
+ // Bu hooku 3 bölümde inceleyebiliriz.
+//  useEffect(() => {
+//    first ()  1- Veri çekme ve güncelleme (fetch-localstorage)
+ 
+//    return () => { 2- component yaşam döngünüsü durdurma /kaldırma
+//      second
+//    }
+//  }, [third]) 3- bağımlılıklara göre tekrar  çalışır eğer bağımlılık varsa sayfa render olunca tekrar çalışır.
+ 
 
   return (
     <div>
-        <ListUser user={user}/>
+      {/*state'te tutulan user bilgisini child componente gönderiyorum.*/}
+      <ListUser user={user}/>
+
+      
     </div>
-  )
+  );
 }
 
 export default UseEffectExample
