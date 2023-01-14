4c301af3d0df875b6e1f5536c1d98cda444cf596
Ali <ali.ozgur@example.com>
2023-01-15 00:32:49 +0300

    edit list component

--- src/components/ListUser.jsx
+++ src/components/ListUser.jsx
@@ -1,19 +1,28 @@
 import React from 'react'
 
+// gelen kullanıcı datasını props olarak karşıladım.
 const ListUser = ({user}) => {
 
   return (
     <div>
         <h3>Github User</h3>
         <ul className='users'>
+            {/*Gelen veri yi map fonkiyonu ile dönüyorum*/}
             {user.map((user)=>{
+                /*gelen veriden DOM  da göstereceğim bilgileri destructuring ediyorum.*/
                 const { id, login, avatar_url, html_url } = user;
                 return (
                   <li key={id}>
                     <img src={avatar_url} alt={login} />
                     <div>
                       <h4>{login}</h4>
-                      <a href={html_url}>profile</a>
+                      <a
+                        href={html_url}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        Profile
+                      </a>
                     </div>
                   </li>
                 );
